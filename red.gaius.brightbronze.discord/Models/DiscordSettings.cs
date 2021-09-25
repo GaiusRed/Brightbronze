ec08450da5d22b6cd9e346611b0477af0a325255
Erik Gaius Capistrano <erik@gaius.red>
2021-09-25 23:24:53 +0800

    Implement Discord.Net-Labs Implement Echo Module

---
+++ red.gaius.brightbronze.discord/Models/DiscordSettings.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace red.gaius.brightbronze.discord.Models
+{
+    public class DiscordSettings
+    {
+        public string Token { get; set; }
+        public int Delay { get; set; }
+        public char DefaultPrefix { get; set; }
+    }
+}
