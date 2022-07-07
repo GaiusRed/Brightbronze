23b37fd42d9ce23c551a5d0013be8f4acc29670d
GaiusRed <erik@gaius.red>
2022-07-08 00:08:30 +0800

    Remove old resources and script id.

--- red.gaius.brightbronze.core/Models/Resources/Script.cs
+++ red.gaius.brightbronze.core/Models/Resources/Script.cs
@@ -2,7 +2,6 @@ namespace red.gaius.brightbronze.core.Models
 {
     public class Script
     {
-        public string id { get; set; }
         public string name { get; set; }
         public string script { get; set; }
     }
